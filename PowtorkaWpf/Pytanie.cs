9d628c029587bdceda8a004878d6651ddd5c0dee
Piotr Dopadlik <sivy.biznes@gmail.com>
2022-10-07 11:35:31 +0200

    first commit

---
+++ PowtorkaWpf/Pytanie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowtorkaWpf
+{
+    public class Pytanie
+    {
+        public int numerPytania {get; set;}
+        public string tresc { get; set; }
+        public List<string> odpowiedzi { get; set; }
+        public int odpowiedzOk { get; set; }
+
+        public Pytanie(int numerPytania, string tresc, List<string> odpowiedzi, int odpowiedzOk)
+        {
+            this.numerPytania = numerPytania;
+            this.tresc = tresc;
+            this.odpowiedzi = odpowiedzi;
+            this.odpowiedzOk = odpowiedzOk;
+        }
+    }
+}
